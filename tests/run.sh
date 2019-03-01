2f746db4c2ca94e8fb668188eecf5338b64253d2
Kees <kees@fastmail.com>
2019-03-01 22:52:23 +0100

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-zypper -n install git ansible
+zypper -n install git ansible python-xml
 
 mkdir /etc/ansible/roles
 cd /etc/ansible/roles || exit 1
 git clone https://github.com/remmelz/ansible-role-opensuse-lxqt.git
 
 cd ansible-role-opensuse-lxqt || exit 1
-ansible-playbook -vvv -c local -i '127.0.0.1,' ./tests/run.yml
+ansible-playbook -c local -i '127.0.0.1,' ./tests/run.yml
 
