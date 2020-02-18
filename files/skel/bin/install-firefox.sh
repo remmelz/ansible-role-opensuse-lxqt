4c77932ba964d62e5586391ee5fb07a38c633b49
Kees <kees@fastmail.com>
2020-02-18 23:45:34 +0100

    Update install-firefox.sh

--- files/skel/bin/install-firefox.sh
+++ files/skel/bin/install-firefox.sh
@@ -29,18 +29,18 @@ fi
 
 mkdir -p applications
 echo "Creating application file..."
-echo '[Desktop Entry]' > applications/firefox.desktop
-echo 'Categories=Network;WebBrowser;GTK;' >> applications/firefox.desktop
-echo 'Encoding=UTF-8' >> applications/firefox.desktop
-echo 'Name=My Firefox' >> applications/firefox.desktop
-echo 'GenericName=Web Browser' >> applications/firefox.desktop
-echo 'Comment=Web Browser' >> applications/firefox.desktop
-echo 'Exec=~/.local/share/firefox/firefox %u' >> applications/firefox.desktop
-echo 'Icon=firefox' >> applications/firefox.desktop
-echo 'Terminal=false' >> applications/firefox.desktop
-echo 'StartupNotify=true' >> applications/firefox.desktop
-echo 'MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;application/x-xpinstall;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;' >> applications/firefox.desktop
-echo 'Type=Application' >> applications/firefox.desktop
+echo '[Desktop Entry]' > applications/myfirefox.desktop
+echo 'Categories=Network;WebBrowser;GTK;' >> applications/myfirefox.desktop
+echo 'Encoding=UTF-8' >> applications/myfirefox.desktop
+echo 'Name=My Firefox' >> applications/myfirefox.desktop
+echo 'GenericName=Web Browser' >> applications/myfirefox.desktop
+echo 'Comment=Web Browser' >> applications/myfirefox.desktop
+echo 'Exec=~/.local/share/firefox/firefox %u' >> applications/myfirefox.desktop
+echo 'Icon=web-browser' >> applications/myfirefox.desktop
+echo 'Terminal=false' >> applications/myfirefox.desktop
+echo 'StartupNotify=true' >> applications/myfirefox.desktop
+echo 'MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;application/x-xpinstall;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;' >> applications/myfirefox.desktop
+echo 'Type=Application' >> applications/myfirefox.desktop
 
 rm -f firefox-latest.tar.bz2
 
