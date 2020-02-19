2da8345afb534423c636499cb252ff09a0878e68
Kees <kees@fastmail.com>
2020-02-19 22:31:03 +0100

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -8,5 +8,5 @@ cd /etc/ansible/roles || exit 1
 git clone https://github.com/remmelz/ansible-role-opensuse-lxqt.git
 
 cd ansible-role-opensuse-lxqt || exit 1
-ansible-playbook -c local -i '127.0.0.1,' ./tests/run.yml
+ansible-playbook -c local -i 'localhost,' ./tests/run.yml
 
