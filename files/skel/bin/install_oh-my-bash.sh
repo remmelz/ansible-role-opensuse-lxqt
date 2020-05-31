2860ac99c2e5bb28227b8c7a920aeffc4bb2edf9
Kees <kees@fastmail.com>
2020-05-31 22:24:16 +0200

    Create install_oh-my-bash.sh

---
+++ files/skel/bin/install_oh-my-bash.sh
@@ -0,0 +1,9 @@
+# Oh-My-Bash is an open source, community-driven framework for managing your BASH configuration. 
+# It comes bundled with a ton of helpful functions, helpers, plugins, themes, and a few things 
+# that make you shout... If you heard about Oh-My-Zsh before, this project is a shameless 
+# fork of that one, but in "Bourne Again Shell".
+
+curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh | bash -x
+
+
+
